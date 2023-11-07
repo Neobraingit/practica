7ced1a2da9f3560b59ae366b726fa02bb9cc6c38
Neobraingit <Maervca2@icloud.com>
2023-11-07 20:02:49 +0100

    Grados

---
+++ 03_Conversor_km.py
@@ -0,0 +1,9 @@
+ # Conversor de unidades: Convierte entre diferentes unidades, como kilómetros a millas o grados Celsius a Fahrenheit.
+ 
+ # Grados centígrados = (grados Fahrenheit − 32) × 5/9
+ 
+grados_celsius = float(input('Introduce los grados celsius: '))
+
+grados_Fahrenheit = (grados_celsius * 1.8) + 32
+
+print (grados_Fahrenheit)
