37cf2a87e3af13e4a01d767a75d5d4b05e056170
Neobraingit <Maervca2@icloud.com>
2023-11-09 17:41:35 +0100

    Calculadora_cambio

---
+++ 16_Calculadora_cambio.py
@@ -0,0 +1,6 @@
+precio = float(input('Introduce el precio: '))
+cantidad = float(input('Dime cuanto dinero tienes para pagar: '))
+
+cambio = cantidad - precio
+
+print ('El cambio de lo que has comprado es de {} euros'.format (cambio))
