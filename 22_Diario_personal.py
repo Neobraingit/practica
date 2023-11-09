d2609cf8d322c3039dac2ec38e90fba48c8aa6f0
Neobraingit <Maervca2@icloud.com>
2023-11-09 19:54:23 +0100

    Diario_personal

---
+++ 22_Diario_personal.py
@@ -0,0 +1,9 @@
+
+print ('Este va a ser tu diario personal...')
+entradas = []
+
+while True:
+  añadir_entrada = input('Introduce un entrada: ')
+  entradas.append(añadir_entrada)
+  entradas.sort()
+  print (entradas)
