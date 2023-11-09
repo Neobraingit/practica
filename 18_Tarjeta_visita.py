7e2fef1facf6ddc93737c54232ec5d1803365ccc
Neobraingit <Maervca2@icloud.com>
2023-11-09 17:54:03 +0100

    Tarjeta_visita

---
+++ 18_Tarjeta_visita.py
@@ -0,0 +1,8 @@
+
+print ('Vamos a completar unnos datos para tu tarjeta de visita....')
+nombre = input('¿Como es tu nombre? ')
+edad = int(input('Dime tu edad: '))
+direccion = input('Dime donde vives: ')
+
+
+print ('Nombre: {} -- Edad: {} -- Dirección: {} '.format(nombre, edad, direccion))
