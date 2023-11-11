080f87d22aa14bb8265e8fa8e86ca467ec9b4e09
Neobraingit <Maervca2@icloud.com>
2023-11-11 17:40:38 +0100

    Gerador_poesía

---
+++ 35_Generador_poesia.py
@@ -0,0 +1,9 @@
+
+import random
+
+palabras = ['sol', 'nubes', 'calor']
+palabras2 = ['perro', 'gato', 'canario']
+palabras3 = ['bonito', 'feo', 'normal']
+
+verso = random.choice(palabras) + ' ' + random.choice(palabras2) + ' ' + random.choice(palabras3)
+print (verso)
