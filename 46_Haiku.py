812e9cc3cddbd34518c1affddc7a53ac79681b28
Neobraingit <Maervca2@icloud.com>
2023-11-11 20:45:39 +0100

    Haiku

---
+++ 46_Haiku.py
@@ -0,0 +1,5 @@
+import random
+haikus = ['Un pedo huele mal',' Un perro siempre espera por su dueño', 'Lo que siembras recogerás' ]
+nombre = input('Escribe tu nombre: ')
+
+print (random.choice(haikus) + ' ' + nombre)
