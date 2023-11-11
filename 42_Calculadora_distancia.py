58b01b36bf3102fd150d928c0d011b2b073352f2
Neobraingit <Maervca2@icloud.com>
2023-11-11 20:03:37 +0100

    Calculadora_distancias

---
+++ 42_Calculadora_distancia.py
@@ -0,0 +1,10 @@
+
+ciudad1 = input('Dime el nombre de la ciudad: ')
+km = float(input('¿A qué distancia está? '))
+ciudad_actual = input('Dime tu ciudad actual: ')
+
+print (ciudad1)
+print (km)
+print (ciudad_actual)
+
+print ('La ciudad {} está a {} km de {}'.format(ciudad1, km, ciudad_actual))
