3dc15b0e562e2fbe059c763a14bdd1c1834c14b9
Neobraingit <Maervca2@icloud.com>
2023-11-11 20:48:30 +0100

    Porcentaje

---
+++ 49_Porcentaje.py
@@ -0,0 +1,6 @@
+
+precio = float(input('Introduce el precio del producto: '))
+porcentaje_descuento = float(input('Introduce el porcentaje a descontar: '))
+
+Total_descontado = (precio * porcentaje_descuento) / 100
+print (Total_descontado)
