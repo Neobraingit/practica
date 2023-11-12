f218ac1ad8efdcadb7b80df7d67fc199dc55e6e6
Neobraingit <Maervca2@icloud.com>
2023-11-12 10:39:37 +0100

    Cumpleaños

---
+++ 51_Tarjeta_cumpleaños.py
@@ -0,0 +1,8 @@
+
+print ('Vamos a cantar la canción de cumpleaños feliz')
+
+cancion1 = 'Cumpleaños feliz '
+cancion2 = 'te deseamos todos '
+nombre = input('Introduce el nombre: ')
+
+print (cancion1 * 2 + cancion2 + nombre)
