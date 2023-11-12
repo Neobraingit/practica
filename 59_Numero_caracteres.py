d234e0bd3679a4be9c9a125f016841e944dd5293
Neobraingit <Maervca2@icloud.com>
2023-11-12 20:00:29 +0100

    Funcion_numero_caracteres

---
+++ 59_Numero_caracteres.py
@@ -0,0 +1,10 @@
+
+archivo = 'archivo.txt'
+
+def numero_caracteres(archivo):
+    with open (archivo, 'r') as miarchivo:
+        contenido = miarchivo.read()
+        print (len(contenido))
+        
+        
+numero_caracteres(archivo)
