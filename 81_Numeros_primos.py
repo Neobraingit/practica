2a979fbd97c21ed1f9605f6e90790cd71dc3f9b9
Neobraingit <Maervca2@icloud.com>
2023-11-18 16:20:16 +0100

    Numeros_primos

---
+++ 81_Numeros_primos.py
@@ -0,0 +1,13 @@
+
+def numeros_primos(num1, num2):
+    if num1 % 2 == 0:
+        print ('Es un número primo')
+    elif num2 % 2 == 0:
+        print ('Es un número primo')
+    else:
+        print ('No son primos')
+        
+        
+        
+        
+numeros_primos(2,4)
