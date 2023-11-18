1cdd0e314223f7aa6849ee9ead76c224d3173880
Neobraingit <Maervca2@icloud.com>
2023-11-18 16:25:24 +0100

    Función_palíndromo

---
+++ 82_Palindromo.py
@@ -0,0 +1,9 @@
+
+def palindromo(cadena):
+    if cadena [0:] == cadena [::-1]:
+        print ('Es un palíndromo¡¡')
+    else:
+        print ('No es un palíndromo¡¡')
+        
+    
+palindromo('qqqqqqqq')
