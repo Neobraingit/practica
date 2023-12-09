142e1c1423d63b20e53752c7d537aa28ffec3497
Neobraingit <Maervca2@icloud.com>
2023-12-09 16:35:14 +0100

    practica

---
+++ 96_Calculadora.py
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env python3
+
+# Crea una función que tome dos números y realice operaciones básicas como suma, resta, multiplicación y división.
+
+def calculadora(num1, num2):
+    print (num1 + num2)
+    print (num1 - num2)
+    print (num1 * num2)
+    print (num1 / num2)
+    
+    
+calculadora(2,2)
